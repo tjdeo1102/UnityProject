9df260a3bdae8e0b036f344801712f6b8b0ef9a9
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2025-12-24 18:08:53 +0900

    [수정] 플레이어 이동 로직 수정

---
+++ Assets/Scripts/Enum.cs
@@ -0,0 +1,4 @@
+public enum RunType
+{
+	None, Slow, Fast
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Base Component")]
-    [SerializeField] CharacterController controller;
+    [SerializeField] Rigidbody rb;
     [SerializeField] PlayerInput input;
 
 
     [Header("Move")]
     [SerializeField] float moveSpeed;
-    [SerializeField] float moveMaxVelocity;
-    [SerializeField] float rotateSpeed;
-    [SerializeField] float runSpeed;
+    [SerializeField] float slowRunSpeed;
+    [SerializeField] float slowRunTransition;
+    [SerializeField] float fastRunSpeed;
 
     [Header("Jump")]
     [SerializeField] float jumpPower;
@@ -36,77 +30,105 @@ public class PlayerMovement : MonoBehaviour
     [Header("Animation")]
     [SerializeField] Animator animator;
 
+
+    float moveTimer = 0f;
+    float speed = 0f;
     public bool isOtherAction;
 
-
-    private Camera mainCamera;
-    private void Awake()
+    Camera mainCamera;
+    
+    void Awake()
     {
         mainCamera = Camera.main;
     }
     void FixedUpdate()
     {
-        if (input != null && controller != null && isOtherAction==false)
+        if (isOtherAction==false)
         {
             Move();
             Jump();
         }
+
         isGround = IsGround();
-        GravityUpdate();
+        // GravityUpdate();
 
         AnimationUpdate();
     }
 
-    #region 이동 관련 기능
+    #region Move
     void Move()
     {
         Vector2 move = input.actions["Move"].ReadValue<Vector2>();
-        // 이미 달리는 도중에 점프하는 경우에 대해서는 기존 속력 보전한 점프
-        // 점프 중에 달리는 경우는 뛰기 전 속력으로 유지
+        var run = false;
+        // Ground Check
         if (isGround == true)
-            isRun = input.actions["Run"].IsPressed();
+            run = input.actions["Run"].IsPressed();
 
         if (move == Vector2.zero)
         {
             IsWalk = false;
+            moveTimer = 0f;
             return;
         }
         
         Vector3 dir = new Vector3(move.x, 0, move.y).normalized;
-        // 카메라가 바라보는 방향이 앞이 되도록 설정
+        // Move Direction Depends on Camera
         if (mainCamera != null)
         {
-            dir = (mainCamera.transform.forward * dir.z + mainCamera.transform.right * dir.x).normalized;
+            var forward = transform.position - mainCamera.transform.position;
+            forward.y = 0;
+            var right = Vector3.Cross(Vector3.up,forward);
+            dir = (forward * dir.z + right * dir.x).normalized;
         }
-        var rotate = dir + transform.position;
-        transform.LookAt(new Vector3(rotate.x,transform.position.y, rotate.z));
 
-        var velocity = dir * moveSpeed * Time.fixedDeltaTime;
+		// move : fastMove : run
+		if (run)
+        {
+            isRun = RunType.Fast;
+            speed = fastRunSpeed;
+        }
+        else 
+        {
+            if(moveTimer < 0f)
+            {
+                isRun = RunType.Slow;
+                speed = slowRunSpeed;
+                moveTimer = -1f;    
+            }
+            else
+            {
+                isRun = RunType.None;
+                speed = moveSpeed;
+            }
+        }
 
-        if (isRun) velocity *= runSpeed;
+        var moveDir = speed * Time.fixedDeltaTime * dir;
 
-        controller.Move(velocity);
-        IsWalk = true;
+        rb.MovePosition(rb.position + moveDir);
+        transform.rotation = Quaternion.LookRotation(moveDir);
+
+        if (IsWalk == false)
+        {
+            moveTimer = slowRunTransition;
+            IsWalk = true;    
+        }
+        moveTimer -= Time.fixedDeltaTime;
     }
     #endregion
-    #region 점프관련 기능
+    #region Jump
     private bool isGround = true;
-    // 이전에 이미 버튼을 누르고 있었는지 확인하는 변수
     private bool isJumpButtonDown = false;
     private bool isJumpDone = false;
     private int objectslayerMaskOnly = -(1 << 7);
     private float jumpButtonDownTime = 0f;
-    private float curJumpVelocity = 0f;
+    private float curJumpPower = 0f;
     private int curJumpType = 0;
     private float curJumpChainTime = 0f;
     void Jump()
     {
         var isJump = input.actions["Jump"].IsPressed();
 
-        // 입력을 누를 때부터 시작
-        // 최대 홀드 시간까지 누른 정도만큼 점프 누른 시간을 저장
-        // 최대 홀드 시간이 지나거나, 버튼을 뗄 경우에만 정해진 파워로 점프
-
+        // Jump Checking
         if (isJump && isGround && isJumpButtonDown == false)
         {
             jumpButtonDownTime = 0f;
@@ -117,7 +139,7 @@ public class PlayerMovement : MonoBehaviour
         {
             if ((jumpButtonDownTime > jumpButtonDownMaxTime || isJump == false) && isJumpDone == false)
             {
-                // 점프가 된 이후 부터, 점프 연계가 가능한 시간 측정
+                // Jump Combo
                 if (curJumpChainTime < jumpChainTime)
                 {
                     curJumpType++;
@@ -133,14 +155,12 @@ public class PlayerMovement : MonoBehaviour
                 }
                 curJumpChainTime = 0f;
 
-                // 최소 속도 보장 + 점프 연계 파워
-                float power = (jumpMinPower + jumpButtonDownTime / jumpButtonDownMaxTime * (1 - jumpMinPower)) * jumpPower + (jumpChainPower * curJumpType);
-                // 초기 속도 설정
-                curJumpVelocity = power;
+                // Jump Power depends on (Jump Press Time, Jump Type)
+                curJumpPower = (jumpMinPower + jumpButtonDownTime / jumpButtonDownMaxTime * (1 - jumpMinPower)) * jumpPower + (jumpChainPower * curJumpType);
+                rb.AddForce(Vector3.up * curJumpPower,ForceMode.Impulse);
 
                 isJumpDone = true;
             }
-            //점프키를 뗀 경우에만 다시 jump키를 누를수 있는 상태가 됨
             jumpButtonDownTime += Time.fixedDeltaTime;
             isJumpButtonDown = isJump;
         }
@@ -148,49 +168,29 @@ public class PlayerMovement : MonoBehaviour
         curJumpChainTime += Time.fixedDeltaTime;
     }
 
-    void GravityUpdate()
-    {
-        // 점프력에 따른 점프
-        // 중력 고려
-        // 떨어지는 속력을 가지지만 땅인 경우, 떨어지지 않음
-        if (isGround && curJumpVelocity < 0f)
-        {
-            // 살짝 뜨는 효과 방지
-            curJumpVelocity = -1f;
-        }
-        else
-        {
-            curJumpVelocity -= gravityScale * Time.fixedDeltaTime;
-        }
-        controller?.Move(Vector3.up * curJumpVelocity * Time.fixedDeltaTime);
-    }
-
     bool IsGround()
     {
-        // 현재 바닥과 충돌중인지 확인
         var cols = Physics.OverlapBox(transform.position, boxSize, transform.rotation, objectslayerMaskOnly);
         return cols.Length > 0;
     }
     #endregion
 
-    #region 애니메이션 관련 기능
+    #region Animation
     private bool IsWalk;
-    private bool isRun;
+    private RunType isRun;
 
     void AnimationUpdate()
     {
-        if (animator == null || controller == null) return;
+        if (animator == null) return;
 
-        // 이동 업데이트
         animator.SetBool("Walk", IsWalk);
-        animator.SetBool("Run", isRun);
+        animator.SetInteger("RunType", (int)isRun);
+        animator.SetFloat("Velocity", speed);
 
-        // 점프 업데이트
         animator.SetInteger("JumpType", curJumpType);
-        animator.SetFloat("JumpVelocity",curJumpVelocity);
+        animator.SetFloat("JumpVelocity",curJumpPower);
         animator.SetBool("IsGround", isGround);
 
-        // Other Ataction
         animator.SetBool("OtherAction", isOtherAction);
     }
     #endregion
